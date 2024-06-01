80861cfaa466f901a349879d864525972c39b013
Asad <syedzadaaa42@gmail.com>
2024-06-01 20:57:23 +0100

    Added Chapter one practice set

--- Chapter 1.py
+++ Chapter 1.py
@@ -1,1 +1,11 @@
-print("Hellow World")
+print("Hello World")
+
+#Write a program to print twinkle twinkle little star in python  
+print("Twinkle Twinkle Little star, how i wonder what you are")
+print("======================================================")
+#Use REPLE and print the table of 5 using it:
+for i in range(1,11):  #This is a for loop where i is in range between 1 and 10
+    print(f"5 x {i} = {5*i}")  #This print the table of 5 against the loop 
+
+a =print()
+a("Asad")
